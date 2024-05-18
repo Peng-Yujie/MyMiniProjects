da0452417242cb0f946363093d6df733aad1bfdc
Yujie <pengyujie.ca@gmail.com>
2024-05-17 19:48:03 -0700

    items adding and deleting

--- fast-react-pizza/src/features/cart/cartSlice.js
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -25,15 +25,9 @@ const cartSlice = createSlice({
     decreaseItemQuantity(state, action) {
       // decrease quantity of item by pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
-      // if quantity is less than 1, delete item
-      if (item.quantity <= 1) {
-        state.cart = state.cart.filter(
-          (item) => item.pizzaId !== action.payload
-        );
-        return;
-      }
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -48,4 +42,17 @@ export const {
   decreaseItemQuantity,
   clearCart,
 } = cartSlice.actions;
+
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+// can be optimized by using `reselect` library
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,31 @@
-import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import Button from '../../ui/Button';
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const dispatch = useDispatch();
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
+  const handleAddToCart = () => {
+    // create a new pizza obj
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice,
+    };
+    // dispatch addItem action
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,8 +47,20 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-
-          <Button type="small">Add to cart</Button>
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
